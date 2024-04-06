ea80f958629930624ed37869d82321676667c132
Alexodius <alex.ostapenko2003@gmail.com>
2024-04-06 15:01:45 +0300

    Добавил кринжа

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function submitData(e) {
 	/* Для работы с формой и вывода результата */
-	e.preventDefault();
-	const formData = new FormData(e.target);
-	const formProps = Object.fromEntries(formData);
-	const res = calculate(formProps.sum, formProps.day, formProps.percent, formProps.isCapital);
-	document.querySelector('.result').innerHTML = `Ваш доход: ${res} руб`;
+	e.preventDefault()
+	const formData = new FormData(e.target)
+	const formProps = Object.fromEntries(formData)
+	const res = calculate(formProps.sum, formProps.day, formProps.percent, formProps.isCapital)
+	document.querySelector('.result').innerHTML = `Ваш доход: ${res} руб`
 }
 
 function calculate(sum, day, percent, isCapital) {
-	/* TODO: Нужно написать данную функцию */
-
-
-	/* TODO: Вернуть получившееся значение */
+	if (isCapital === 'no') {
+		return (sum * (day / 365) * percent) / 100
+	} else {
+		return sum * (1 + percent / 100) ** (day / 365) - sum
+	}
 }
