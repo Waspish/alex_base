4ac8cef32e91e4cc8b562a5c0569a0bebec53ac7
Alexodius <alex.ostapenko2003@gmail.com>
2024-04-06 15:16:55 +0300

    Исправил недочеты

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 function submitData(e) {
 	/* Для работы с формой и вывода результата */
-	e.preventDefault()
-	const formData = new FormData(e.target)
-	const formProps = Object.fromEntries(formData)
-	const res = calculate(formProps.sum, formProps.day, formProps.percent, formProps.isCapital)
-	document.querySelector('.result').innerHTML = `Ваш доход: ${res} руб`
+	e.preventDefault();
+	const formData = new FormData(e.target);
+	const formProps = Object.fromEntries(formData);
+	const res = calculate(
+		formProps.sum,
+		formProps.day,
+		formProps.percent,
+		formProps.isCapital
+	);
+	document.querySelector('.result').innerHTML = `Ваш доход: ${res.toFixed(
+		2
+	)} руб`;
 }
 
 function calculate(sum, day, percent, isCapital) {
 	if (isCapital === 'no') {
-		return (sum * (day / 365) * percent) / 100
+		return (sum * (day / 365) * percent) / 100;
 	} else {
-		return sum * (1 + percent / 100) ** (day / 365) - sum
+		return sum * (1 + percent / 100 / 365) ** day - sum;
 	}
 }
